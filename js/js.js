1a74c3a2ba56f77cc37e43880d482ffa9dff6cd5
Yasvet <vipmaster@tut.by>
2022-05-07 13:47:38 +0300

    ver 0.1 - добавлены бонусные ходы - добавлен подсчет очков - добавден перезапуск игры после завершения

--- js/js.js
+++ js/js.js
@@ -1,119 +1,200 @@
+$(document).ready(function () {
 
-$(document).ready(function() {
+    //TODO Если 2 одинаковая картинка до конца не раскрылась и нажать на 3-ю, очки начисляются, но картинки закрываются обратно
 
-let m=[],
-    n=[];
-let area=4;
-const position_1 = ['top', 'center', 'bottom'];
-const position_2 = ['right', 'center', 'left'];
+    var m = [], n = [], area = 4, timeToOne = 5,//время на 1 ход
+        box = $('.m_content ul'), boxLi, selectSize = $('.size'), timer_timeout;
+    const position_1 = ['top', 'center', 'bottom'];
+    const position_2 = ['right', 'center', 'left'];
 
     Array.prototype.random = function () {
-        return this[Math.floor((Math.random()*this.length))];
+        return this[Math.floor((Math.random() * this.length))];
     }
 
-    function start(area){
-        $('.m_content ul').html('');
 
-        icount=area*area;
-        for(i=0;i<icount/2;i++){
-            n = m.push(i+1);
-            n = m.push(i+1);
+    function start(area) {
+        box.html('');
+        m = [];
+        let icount = area * area;
+        for (i = 0; i < icount / 2; i++) {
+            n = m.push(i + 1);
+            n = m.push(i + 1);
         }
 
-        var newcount = icount-2;
-        var newMass = [];
-        for(i=0;i<icount;i++){
-            newm = m;
-            rand=Math.floor((Math.random()*newcount)+1);
-            removed = newm.splice(rand,1);
+        let newcount = icount - 2;
+        let newMass = [];
+
+        for (let i = 0; i < icount; i++) {
+            let newm = m, rand = Math.floor((Math.random() * newcount) + 1), removed = newm.splice(rand, 1);
             n = newm.shift();
-            t = newm.push(n);
+            var t = newm.push(n);
             n = newMass.push(removed[0]);
-            $('.m_content ul').append('<li><div class="li"><div class="bg"></div><div class="block" style="background-image: url(./img/pet/'+removed[0]+'.png)">'+removed[0]+'</div></div></li>');
+            box.append('<li><div class="li"><div class="bg"></div><div class="block" style="background-image: url(./img/pet/' + removed[0] + '.png)">' + removed[0] + '</div></div></li>');
             newcount--;
         }
 
-        $('.m_content ul').css('width', $('.m_content ul li').outerWidth()*area);
+        boxLi = $('.m_content li');
 
+        box.css('width', boxLi.outerWidth() * area);
+        box.css('background-position', position_1.random() + ' ' + position_2.random());
 
-        $('.m_content ul').css('background-position', position_1.random()+' '+position_2.random());
+        viewElements();
+        addBonusPoints();
 
     }
 
 
-    $('.size').change(function(){
+    selectSize.change(function () {
         opt = $(this).val();
-        val = $(this).find('option').eq(opt-1).text();
+        val = $(this).find('option').eq(opt - 1).text();
         start(val);
-        viewElements();
+
     });
 
-    $('.m_content ').on('click', '.li', function(e){
-        e.preventDefault();
-        ithis = $(this);
-        parent = ithis.closest('.m_content');
-        if(!(ithis.hasClass('clear') )){
-            if( !( parent.hasClass('locked') )){
-                ithis.addClass('active');
+    var firstEL, lastEL;
 
-                if( $('.m_content .li.active').size() == 2 ){
-                    $('.li.active:first').addClass('first');
-                    $('.li.active:last').addClass('last');
-                    setTimeout(function(){ check(); }, 500);
+    $('.m_content ').on('click', '.li', function (e) {
+        e.preventDefault();
+        el = $(this);
+        parent = el.closest('.m_content');
+        if (!(el.hasClass('clear'))) {
+            if (!(parent.hasClass('locked'))) {
+                el.addClass('active');
+                if (boxLi.find('.active').size() == 2) {
+                    firstEL = $('.li.active:first');
+                    lastEL = $('.li.active:last');
+                    firstEL.addClass('first');
+                    lastEL.addClass('last');
+                    setTimeout(function () {
+                        check(firstEL, lastEL);
+
+                        let bonusPoints = parseInt($('.bonusScoreCounter').text());
+                        if (bonusPoints) {
+                            $('.bonusScoreCounter').html(bonusPoints - 1);
+                        }
+
+
+                    }, 500);
+
                 }
-                if( $('.m_content .li.active').size() == 3 ){
+                if (boxLi.find('.active').size() == 3) {
                     $('.li.first, .li.last').removeClass('active first last');
                 }
             }
         }
     });
 
-    function check(){
-        if($('.li.active.first').text()==$('.li.active.last').text()){
+    function check(firstEL, lastEL) {
+        if (firstEL.text() == lastEL.text()) {
             $('.li.first, .li.last').addClass('clear').animate({
                 fontSize: "0em"
             }, 500);
+            getScore(firstEL);
         }
-        setTimeout(function(){
+        setTimeout(function () {
             $('.li.first, .li.last').removeClass('active first last');
         }, 600);
         checkCompleate();
     };
 
-    function checkCompleate(){
-        if(($('.li.clear').size()==$('.li').size())){
-            $('.m_content li').animate({
-                opacity:0
+    function checkCompleate() {
+        if (($('.li.clear').size() == $('.li').size())) {
+            boxLi.animate({
+                opacity: 0
             }, 700);
+
+            $('.gameBox').append('<div class="btnRestart"></div>');
+            $('.m_content').find('.btnRestart').on('click', function () {
+                document.location.reload();
+            });
         }
     };
 
-    function viewElements(){
-        isize =  $('.m_content li').size();
+    function viewElements() {
+        isize = $('.m_content li').size();
 
-        var i=0;
+        var i = 0;
 
 
-        setInterval(function(){
+        setInterval(function () {
 
             asd(i);
             i++
         }, 300);
 
-        function asd(i){
+        function asd(i) {
             $('.m_content li').eq(i).find('.li').addClass('active');
-            setTimeout(function(){
+            setTimeout(function () {
                 $('.m_content li').eq(i).find('.li').removeClass('active');
             }, 700);
         }
+    }
+
+// Верия без анимации открытия плиток
+    // function viewElements() {
+    //
+    //     $('.counter ').append('<span class="timer"></span>');
+    //     isize = boxLi.size();
+    //     var i = 0;
+    //
+    //     function showallel() {
+    //         showelem = setTimeout(function () {
+    //             showel(i);
+    //             i++;
+    //
+    //             setTimeout(function () {
+    //                 showallel();
+    //             }, 100);
+    //         }, 200);
+    //     }
+    //
+    //     showallel();
+    //
+    // }
+
+    function showel(i) {
+        boxLi.eq(i).find('.li').addClass('showElement');
+        setTimeout(function () {
+            boxLi.eq(i).find('.li').removeClass('showElement');
+        }, 700);
+    }
 
 
+    function getScore(el) {
+        iScore = $('.playerScoreCounter');
+        // bonusVal = parseInt(el.find('.block').text()); // Очки каждой клетки
+        bonusScore = parseInt($('.bonusScoreCounter').text());
 
+        newval = bonusScore;
+
+        oldval = parseInt(iScore.text());
+        newScore = $('.NewPlayerScore');
+        newScore.text(newval).show();
+        newScore.animate({
+            right: "+=150", top: "+=300", fontSize: "20px"
+        }, 1000);
+
+        if (parseInt($('.bonusScoreCounter').text()) > 0) {
+            setTimeout(function () {
+                newScore.hide();
+                iScore.text(newval + oldval);
+                newScore.css({'top': '-290px', 'right': '-100px', 'font-size': '50px'});
+            }, 1000);
+        }
 
     }
-    $(window).load(function(){
+
+    function addBonusPoints() {
+        $('.m_content').prepend('<div class="bonusScoreBox"><div class="bonusScore"><div class="bonusScoreFill">Бонусные ходы: <span class="bonusScoreCounter">' + $('.m_content li').size() + '</div></span></div></div>');
+    }
+
+    (function addPlayerScore() {
+        $('.m_content').append('<div class="playerScoreBox"><div class="playerScore"><div class="playerScoreFill">Player Score: <span class="playerScoreCounter">0</span><span class="NewPlayerScore">0</span></div></div></div>');
+    }());
+
+
+    $(window).load(function () {
         start(area);
-        viewElements();
     });
 
-})
+});
